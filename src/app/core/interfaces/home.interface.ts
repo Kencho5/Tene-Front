fd68506d799a483981443448cdd742100a5466ce
Kencho5 <giokenchadze@gmail.com>
2025-09-23 12:56:11 +0400

    add: cards

---
+++ src/app/core/interfaces/home.interface.ts
@@ -0,0 +1,6 @@
+export interface HomeCard {
+  image: string;
+  title: string;
+  subtext: string;
+  button: string;
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { Image } from '@shared/components/ui/image/image';
+import { homeCards } from '@utils/homeCards';
 
 @Component({
   selector: 'app-home',
   imports: [Image],
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  homeCards = homeCards;
+}
