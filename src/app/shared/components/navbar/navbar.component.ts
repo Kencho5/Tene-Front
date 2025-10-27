1616f0ffff6268a5cd9b9240caa48f8dc9dbfff8
Kencho5 <giokenchadze@gmail.com>
2025-10-27 10:06:49 +0400

    add: mobile menu

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SharedModule } from '../../shared.module';
+import { navUrls } from '@utils/navUrls';
 
 @Component({
   selector: 'app-navbar',
   imports: [SharedModule],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  mobileMenuOpen = signal(false);
+  navUrls = navUrls;
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen.update((value) => !value);
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen.set(false);
+  }
+}
---
+++ src/app/utils/navUrls.ts
@@ -0,0 +1,36 @@
+import { NavUrl } from '@core/interfaces/navbar.interface';
+
+export const navUrls: NavUrl[] = [
+  {
+    url: 'products',
+    name: 'პროდუქცია',
+  },
+  {
+    url: 'bins',
+    name: 'ურნები',
+  },
+  {
+    url: 'coins',
+    name: 'ტენე ქულები',
+  },
+  {
+    url: 'recycle',
+    name: 'გადადნობა',
+  },
+  {
+    url: 'earn',
+    name: 'გამოიმუშავე თანხა',
+  },
+  {
+    url: 'about',
+    name: 'ჩვენს შესახებ',
+  },
+  {
+    url: 'blog',
+    name: 'ბლოგი',
+  },
+  {
+    url: 'contact',
+    name: 'კონტაქტი',
+  },
+];
