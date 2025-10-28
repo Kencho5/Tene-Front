657b7eefff54c63203277ba18d146330d8559e8c
Kencho5 <giokenchadze@gmail.com>
2025-10-28 13:24:44 +0400

    add: rank cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -84,6 +84,15 @@ module.exports = {
       boxShadow: {
         low: "0px 1px 8px 0px #14141414, 0px 0px 1px 0px #1414141F",
       },
+      container: {
+        center: true,
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+        },
+      },
     },
   },
   plugins: [],
