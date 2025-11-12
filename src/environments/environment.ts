1b6614696aa120c197735b6f8af4a47d58a9b1a4
Kencho5 <giokenchadze@gmail.com>
2025-11-12 10:04:00 +0400

    update: api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://ec2-63-176-52-134.eu-central-1.compute.amazonaws.com',
+  apiUrl: 'https://api.tenetest.com',
   google_client_id:
     '1094512636497-fkebm4p0rt83p0m4ohbrhgu6liq481ts.apps.googleusercontent.com',
 };
