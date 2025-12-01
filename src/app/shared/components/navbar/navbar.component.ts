b0070dbf4a15ef9344315a2296bc7e00bc690b09
Kencho5 <giokenchadze@gmail.com>
2025-12-01 14:29:47 +0400

    add: cart item number to navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { SharedModule } from '../../shared.module';
 import { navUrls } from '@utils/navUrls';
 import { AuthService } from '@core/services/auth/auth-service.service';
+import { CartService } from '@core/services/products/cart.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,7 @@ import { AuthService } from '@core/services/auth/auth-service.service';
 })
 export class NavbarComponent {
   readonly authService = inject(AuthService);
+  readonly cartService = inject(CartService);
 
   mobileMenuOpen = signal(false);
   navUrls = navUrls;
