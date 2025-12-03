49f760e01f8ae744b019ea44768fcae3e989b3a4
Kencho5 <giokenchadze@gmail.com>
2025-12-03 11:58:36 +0400

    add: empty cart state

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '@core/services/products/cart.service';
+import { ImageComponent } from '@shared/components/ui/image/image.component';
 import { SharedModule } from '@shared/shared.module';
 
 @Component({
   selector: 'app-cart',
-  imports: [SharedModule],
+  imports: [SharedModule, ImageComponent],
   templateUrl: './cart.component.html',
 })
 export class CartComponent {
-  private readonly cartService = inject(CartService);
+  readonly cartService = inject(CartService);
 }
