9c36be1c053106a93ad79f35dc6783311d353cb1
Kencho5 <giokenchadze@gmail.com>
2025-12-31 14:15:02 +0400

    add: products route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -30,6 +30,13 @@ export const routes: Routes = [
           ),
       },
       {
+        path: 'products',
+        loadComponent: () =>
+          import('./pages/products/products.component').then(
+            (m) => m.ProductsComponent,
+          ),
+      },
+      {
         path: 'products/:product_id',
         loadComponent: () =>
           import('./pages/product/product.component').then(
