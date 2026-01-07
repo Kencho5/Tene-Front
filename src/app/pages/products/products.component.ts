e9fdab89440dddab72dec933d70071cf4a8095cf
Kencho5 <giokenchadze@gmail.com>
2026-01-07 14:55:34 +0400

    add: scroll arrows

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { productCategoriesCards } from '@utils/productsCards';
 
@@ -14,4 +14,24 @@ import { productCategoriesCards } from '@utils/productsCards';
 })
 export class ProductsComponent {
   productCategoriesCards = productCategoriesCards;
+  readonly scrollStates = signal<Record<string, boolean>>({});
+
+  scrollLeft(element: HTMLElement) {
+    element.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+
+  scrollRight(element: HTMLElement) {
+    element.scrollBy({ left: 300, behavior: 'smooth' });
+  }
+
+  onScroll(element: HTMLElement, sectionId: string) {
+    this.scrollStates.update((states) => ({
+      ...states,
+      [sectionId]: element.scrollLeft > 0,
+    }));
+  }
+
+  canScrollLeft(sectionId: string): boolean {
+    return this.scrollStates()[sectionId] || false;
+  }
 }
