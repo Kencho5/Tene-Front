1280a5490448018a5289090bcd638b66f321bce6
Kencho5 <giokenchadze@gmail.com>
2026-01-20 11:13:15 +0400

    add: cards to search results

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,16 +1,30 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ComboboxItems } from '@core/interfaces/combobox.interface';
 import { ProductsService } from '@core/services/products/products.service';
-import { BreadcrumbComponent, BreadcrumbItem } from '@shared/components/ui/breadcrumb/breadcrumb.component';
+import {
+  BreadcrumbComponent,
+  BreadcrumbItem,
+} from '@shared/components/ui/breadcrumb/breadcrumb.component';
 import { DropdownComponent } from '@shared/components/ui/dropdown/dropdown.component';
+import { ProductCardComponent } from '@shared/components/ui/product-card/product-card.component';
 import { SharedModule } from '@shared/shared.module';
 import { map, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
-  imports: [SharedModule, DropdownComponent, BreadcrumbComponent],
+  imports: [
+    SharedModule,
+    DropdownComponent,
+    BreadcrumbComponent,
+    ProductCardComponent,
+  ],
   templateUrl: './search.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -21,7 +35,9 @@ export class SearchComponent {
   private debounceTimer?: number;
   private startTime = 0;
 
-  readonly params = toSignal(this.route.queryParams, { initialValue: {} as Params });
+  readonly params = toSignal(this.route.queryParams, {
+    initialValue: {} as Params,
+  });
   readonly searchTime = signal<number>(0);
 
   readonly breadcrumbItems: BreadcrumbItem[] = [
@@ -36,12 +52,14 @@ export class SearchComponent {
 
   readonly products = toSignal(
     this.route.queryParams.pipe(
-      tap(() => this.startTime = performance.now()),
-      map(params => new URLSearchParams(params).toString()),
-      switchMap(query => this.productsService.searchProduct(query)),
-      tap(() => this.searchTime.set((performance.now() - this.startTime) / 1000))
+      tap(() => (this.startTime = performance.now())),
+      map((params) => new URLSearchParams(params).toString()),
+      switchMap((query) => this.productsService.searchProduct(query)),
+      tap(() =>
+        this.searchTime.set((performance.now() - this.startTime) / 1000),
+      ),
     ),
-    { initialValue: [] }
+    { initialValue: [] },
   );
 
   setParam(key: string, value: string | undefined, debounce = 0): void {
