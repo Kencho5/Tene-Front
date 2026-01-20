8f8dca06379b83deed046b7397c17e87437885cb
Kencho5 <giokenchadze@gmail.com>
2026-01-20 11:22:17 +0400

    fix: product images

--- src/app/shared/components/ui/product-card/product-card.component.ts
+++ src/app/shared/components/ui/product-card/product-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { ProductResponse } from '@core/interfaces/products.interface';
 import { SharedModule } from '@shared/shared.module';
 import { ImageComponent } from '../image/image.component';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'app-product-card',
@@ -9,5 +10,12 @@ import { ImageComponent } from '../image/image.component';
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
+  readonly imageBaseUrl = environment.product_image_url;
   readonly product = input.required<ProductResponse>();
+  readonly productImage = computed(() => {
+    const primaryImage = this.product().images.find((image) => image.is_primary);
+    if (!primaryImage) return '';
+
+    return `${this.imageBaseUrl}/products/${this.product().product.id}/${primaryImage.image_uuid}.jpg`;
+  });
 }
