6d3060a8b800da8e5f9663560ccf8ba6d340327a
Kencho5 <giokenchadze@gmail.com>
2026-03-05 13:14:25 +0400

    update: domains

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,6 @@
 export const environment = {
   environment: 'staging',
   apiUrl: '/api',
-  google_client_id:
-    '1094512636497-fkebm4p0rt83p0m4ohbrhgu6liq481ts.apps.googleusercontent.com',
-  product_image_url: 'https://assets.tenetest.com',
+  google_client_id: '1094512636497-fkebm4p0rt83p0m4ohbrhgu6liq481ts.apps.googleusercontent.com',
+  product_image_url: 'https://assets.tene.ge',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   environment: 'main',
-  apiUrl: 'https://api.tenetest.com',
-  google_client_id:
-    '1094512636497-fkebm4p0rt83p0m4ohbrhgu6liq481ts.apps.googleusercontent.com',
-  product_image_url: 'https://assets.tenetest.com',
+  apiUrl: 'https://api.tene.ge',
+  google_client_id: '1094512636497-fkebm4p0rt83p0m4ohbrhgu6liq481ts.apps.googleusercontent.com',
+  product_image_url: 'https://assets.tene.ge',
 };
