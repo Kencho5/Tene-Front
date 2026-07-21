ea8b9ffd2b6aff372cd1964cb69a46e90753c854
Kencho5 <giokenchadze@gmail.com>
2026-07-21 13:56:39 +0400

    update: payment link form

--- src/app/core/interfaces/admin/payment-link.interface.ts
+++ src/app/core/interfaces/admin/payment-link.interface.ts
@@ -1,41 +1,9 @@
-export interface PaymentLinkItem {
-  product_id: string;
-  product_name: string;
-  color: string | null;
-  quantity: number;
-  price: string;
-}
-
-export interface PaymentLinkIndividualCustomer {
-  customer_type: 'individual';
-  name: string;
-  surname: string;
-}
-
-export interface PaymentLinkCompanyCustomer {
-  customer_type: 'company';
-  organization_type: string;
-  organization_name: string;
-  organization_code: string;
-}
-
-export type PaymentLinkCustomer =
-  | PaymentLinkIndividualCustomer
-  | PaymentLinkCompanyCustomer;
-
 export interface PaymentLinkRequest {
-  customer: PaymentLinkCustomer;
-  email: string;
-  phone_number: string;
-  address: string;
-  city: string | null;
-  region: string | null;
-  details: string | null;
-  delivery_type: string;
-  delivery_time: string;
+  price: string | null;
+  email: string | null;
+  phone_number: string | null;
+  address: string | null;
   comment: string | null;
-  items: PaymentLinkItem[];
-  price: string;
 }
 
 export interface PaymentLinkResponse {
@@ -43,34 +11,10 @@ export interface PaymentLinkResponse {
   checkout_url: string;
 }
 
-export interface PaymentLinkItemFields {
-  product_id: string;
-  product_name: string;
-  color: string;
-  quantity: number;
-  price: string;
-}
-
 export interface PaymentLinkFields {
-  customer_type: 'individual' | 'company';
-  individual: {
-    name: string;
-    surname: string;
-  };
-  company: {
-    organization_type: string;
-    organization_name: string;
-    organization_code: string;
-  };
+  price: string;
   email: string;
   phone_number: string;
   address: string;
-  city: string;
-  region: string;
-  details: string;
-  delivery_type: string;
-  delivery_time: string;
   comment: string;
-  price: string;
-  items: PaymentLinkItemFields[];
 }
